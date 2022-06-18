212d150945e7a5aa15d2d0d3a00a6c51835d08a5
Chensh <mail2chensh@gmail.com>
2022-06-19 00:26:29 +0800

    Initial Commit

---
+++ MessageFilterTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageFilterTest
+//
+//  Created by Chensh on 2022/6/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
